b8dd5f51af232c390b9d0a44baaa23f17342202a
Aissatou28208 <122581840+Aissatou28208@users.noreply.github.com>
2023-04-05 23:45:44 -0400

    Notre programme de geston des clients des hotels sans aucune action

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GESTION_DES_CLIENTS_HOTELS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
